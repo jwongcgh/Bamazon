61c7a8ece25f7beb0dd25aecd15b559544339b2b
Jorge Wong <JW@Jorges-MBP.attlocal.net>
2017-02-05 16:51:49 -0600

    seeds and schema, readme.md

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Bamazon
 
+## Video demo-link: only Customer and Manager
+
 [bamazonCustomer-VideoLink](https://drive.google.com/drive/folders/0B_imyucfubVZcXlRNTZxQVlwS2M);
 
--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,4 @@
-CREATE DATABASE Bamazon;
-
 USE Bamazon;
-
-CREATE TABLE products (
-item_id INTEGER(100) AUTO_INCREMENT NOT NULL,
-product_name VARCHAR(100) NOT NULL,
-department_name VARCHAR(50) NULL,
-price FLOAT(10,2) NOT NULL,
-stock_quantity INTEGER(10) NULL,
-PRIMARY KEY(item_id)
-);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
@@ -24,3 +13,6 @@ VALUES
 ('Odd Thomas - DVD', 'Movies', 7.07, 35),
 ('Makita 18V XPH102 Cordless 1/2" Hammer Driver-Crill Kit', 'Tools', 129.00, 20);
 
+SELECT * FROM products;
+
+-- UPDATE products SET stock_quantity = 20;
